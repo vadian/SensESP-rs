c2416a5bbc28a272d9b75b5356a7ba18dbf99f0e
Carl Middleton <dev@carlcodes.com>
2024-12-14 18:35:46 -0800

    last commit works, this is wip on sending deltas

--- src/lib/signalk/connect.rs
+++ src/lib/signalk/connect.rs
@@ -1,3 +1,4 @@
+use core::str;
 use core::time::Duration;
 
 use crate::signalk::auth::get_token;
@@ -6,8 +7,10 @@ use esp_idf_svc::io::EspIOError;
 use esp_idf_svc::ws::client::{
     EspWebSocketClient, EspWebSocketClientConfig, WebSocketEvent, WebSocketEventType,
 };
-use log::{error, info};
-use signalk::SignalKStreamMessage;
+use log::{error, info, warn};
+use serde_json::json;
+use signalk::delta::{V1DeltaFormatBuilder, V1UpdateTypeBuilder};
+use signalk::{SignalKStreamMessage, V1DefSource, V1UpdateValue};
 use std::sync::mpsc;
 
 pub fn signalk_server(server_root: &str) -> Result<()> {
@@ -25,26 +28,41 @@ pub fn signalk_server(server_root: &str) -> Result<()> {
     let timeout = Duration::from_secs(10);
     let (tx, _rx) = mpsc::channel::<SignalKStreamMessage>();
 
+    //change this to subscribe=all to get flooded with all the server deltas on terminal :D
     let url = format!("ws://{}/signalk/v1/stream?subscribe=all", server_root);
     let _client = EspWebSocketClient::new(url.as_str(), &config, timeout, move |event| {
-        handle_signalk_server_event(&tx, event)
+        handle_signalk_server_event(event)
     })?;
 
     loop {
-        std::thread::sleep(Duration::from_millis(50));
+        std::thread::sleep(Duration::from_millis(2000));
+        match tx.send(SignalKStreamMessage::Delta(
+            V1DeltaFormatBuilder::default()
+                .add_update(
+                    V1UpdateTypeBuilder::default()
+                        .source(V1DefSource::builder().label("Basic SensESP-rs Sensor example".to_string()).build())
+                        .add_update(V1UpdateValue {
+                            path: "navigation.speedOverGround".to_string(),
+                            value: json!(7.85),
+                        })
+                        .build(),
+                )
+                .build(),
+        )) {
+            Ok(()) => info!("Successfully sent delta."),
+            Err(e) => error!("Error sending delta: {:?}", e),
+        }
     }
 }
 
 fn handle_signalk_server_event(
-    _tx: &mpsc::Sender<SignalKStreamMessage>,
+    //     _tx: &mpsc::Sender<SignalKStreamMessage>,
     event: &Result<WebSocketEvent, EspIOError>,
 ) {
     match event {
         Ok(event) => match event.event_type {
             WebSocketEventType::BeforeConnect => {
                 info!("Websocket before connect");
-                //tx.send(SignalKStreamMessage::Hello(V1HelloBuilder::default().build()))
-                //.ok();
             }
             WebSocketEventType::Connected => {
                 info!("Websocket connected");
@@ -61,12 +79,17 @@ fn handle_signalk_server_event(
             }
             WebSocketEventType::Text(text) => {
                 info!("Websocket recv, text: {text}");
-                if text == "Hello, World!" {
-                    //tx.send(ExampleEvent::MessageReceived).ok();
-                }
             }
             WebSocketEventType::Binary(binary) => {
                 info!("Websocket recv, binary: {binary:?}");
+                let text = match str::from_utf8(binary) {
+                    Ok(t) => Some(t),
+                    Err(e) => {
+                        warn!("Unable to parse binary to ascii: {e:?}");
+                        None
+                    }
+                };
+                info!("Parsed binary: {text:?}")
             }
             WebSocketEventType::Ping => {
                 info!("Websocket ping");
@@ -76,7 +99,7 @@ fn handle_signalk_server_event(
             }
         },
         Err(e) => {
-            error!("{}", e);
+            error!("Error handling event: {:?}", e);
         }
     }
 }
